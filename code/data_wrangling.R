85f27e859329949de8e2f5532e42912b603e2955
86fyko <merles@unis.no>
2024-11-08 16:32:10 +0100

    fixed the data directories to make them tidy with here()

--- code/data_wrangling.R
+++ code/data_wrangling.R
@@ -9,16 +9,20 @@ library("vegan")
 library("reshape2")
 library("taxadb") #used for cleaning the species names
 
-setwd("~/Documents/Uni/Masterarbeit/Data_Analysis_Endalen") #set working directoy to project directory
+#setwd("~/Documents/Uni/Masterarbeit/Data_Analysis_Endalen") #set working directoy to project directory
 
 #### import data####
-EndalenOld<- read.csv("./data/veg/RAW_Excel/ENDALEN_ALLSITES_2003to2015.csv") #removes columns that are not relevant for my data analysis
+datdir <- here("data","veg", "RAW_Excel")
+EndalenOld<- read.csv(paste(datdir,"/ENDALEN_ALLSITES_2003to2015.csv", sep="")) #removes columns that are not relevant for my data analysis
 str(EndalenOld)
 names(EndalenOld)
 Vegetation.df <- EndalenOld[,c(-1,-(6:7), -(10:11), -16)]#removes columns that are not relevant for my data analysis
 # not realy relevant but if I want to keep the rows and columns I can not delete column 6:7 and rename them in this step
 #names(Vegetation.df)[c(5,6)] <- c("COLUMN", "ROW")
 
+
+#create output directory for secondary data:
+datadir_secondary <- here("data","secondaryData")
 
 ####Check that all the species have a functional groups####
 unique(EndalenOld[,c("GFNARROWwalker","SPECIES_NAME")])
@@ -59,7 +63,7 @@ not_vegetation <- c("ROCK","LITTER","SOIL")
 VascularHT <- c("SDECI","GRASS", "FORB","SEVER","SLVASC","RUSH", "SEDGE") # create a vector with the functional groups of the vascular plants
 
 
-fungr<- read.csv("./data/secondaryData/funcgroup2022_list.csv")
+fungr<- read.csv(paste(datadir_secondary,"/funcgroup2022_list.csv", sep=""))
 #write.csv(fungroup2022, "funcgroup2022.csv", row.names=F)
 
 #update the functional groups
@@ -91,7 +95,10 @@ species_info <- unique(Vegetation.dff[,c(8:10)])
 veg_long <- merge(VEG_Abund,species_info)
 
 plot_treatment<- unique(VEG_Abund[,c(2,4)])
-write_csv(plot_treatment, "../data/plot_treatment.csv")
+
+write_csv(plot_treatment, paste(here("data"),"/plot_treatment.csv", sep=""))
+
+
 
 #vascular plants only
 VASCULAR_Abund <-   group_by(vascular.df, SITE,PLOT,SUBSITE,TREATMENT,YEAR) %>%  
@@ -109,7 +116,7 @@ VEG_wide<- pivot_wider(VEG_Abund, values_from=n, names_from = SPECIES_NAME)
 VEG_wide[is.na(VEG_wide)]<-0
 
 
-Endalen2022wide<- read.csv("./data/secondaryData/data2022PerPlot.csv")
+Endalen2022wide<- read.csv(paste(datadir_secondary,"/data2022PerPlot.csv", sep=""))
 
 colnames(VASCULAR_wide) <- gsub(" ", "\\.", colnames(VASCULAR_wide))
 
@@ -140,7 +147,6 @@ allendalen_ordered <- unique(allendalen[, ordered_column_names])
 colnames(allendalen_ordered)
 
 
-
-write.csv(allendalen_ordered,"/Users/ms/Documents/Uni/Masterarbeit/Data_Analysis_Endalen/data/secondaryData/allEndalenAbundance.csv", 
+write.csv(allendalen_ordered,paste(datadir_secondary, "allEndalenAbundance.csv"),
           row.names = FALSE)
 
